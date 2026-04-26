32e2fc4f9547a32b96bd32b78c099dfa4e0b352b
CRi-dev99 <lego.address@outlook.com>
2026-04-26 11:18:09 +0100

    cleaned it up a bit

--- ClientCode/clientv2.py
+++ ClientCode/clientv2.py
@@ -1,31 +1,8 @@
 from email import message
 import socket
-import keyboard
+import keyboard # pip install keyboard - library to detect keystrokes
 
 
-'''def client_program():
-    host = "192.168.0.7"
-    port = 5000  # socket server port number
-
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
-
-    message = input(" -> ")  # take input
-
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
-if __name__ == '__main__':
-    client_program()
-'''
 
 
 
